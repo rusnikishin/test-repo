b79ecbc14ef1585ff15df9eefd6b62536bf744eb
rusnikishin <rus.nikishin@gmail.com>
2015-09-29 21:07:52 +0300

    Rearrange code Add import

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,11 @@
-import java.lang.Integer;
-import java.lang.System;
-
 /*Сделано для проверки работы
 репозитория*/
-public class HelloWorld{
-		public static void main(String[] args){
-			System.out.println("Calculate two numbers:");
-			int one = Integer.valueOf(args[0]);
-			int two = Integer.valueOf(args[1]);
-            int result = one + two;
-			System.out.println(result);
-		}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Calculate two numbers:");
+        int one = Integer.valueOf(args[0]);
+        int two = Integer.valueOf(args[1]);
+        int result = one + two;
+        System.out.println(result);
+    }
 }
